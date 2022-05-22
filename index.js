880297b1a97fc0bb3349abe7a6d55a7caeadbbf3
Alexandra Wenner <alexandrawenner05@gmail.com>
2022-05-22 12:02:39 -0400

    commit tree c9a856a6f46609531e6cc9cac6e7a4c80378e345

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code your solution here
+
+// function superbowlWin(record) {
+//    return (record % record.result === "W")
+// }
+
+
+function superbowlWin(record) {
+    for(const champs of record) {
+        if (champs.result === "W") {
+            return champs.year
+        }
+    }
+}
+
+
+
+//for(const record of game){}
+
+// if  a w is fouind return the year the win occurred
+
+//if no w is found, return undefined
+
+
+// function superbowlWin(win) {
+//     let date - win.find(champs = >champs.result === 'W')
+//         if (date) {
+//             return date.year
+//         } else {
+//             return undefined
+//         }
+// }
